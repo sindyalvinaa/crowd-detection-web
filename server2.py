7de407167fd64f4c6e15a5f6e172372a5f83ba5d
sindyalvinaa <sindyalvina93@gmail.com>
2024-07-19 20:27:51 +0700

    report

--- server2.py
+++ server2.py
@@ -1,4 +1,4 @@
-# Mengimpor modul yang diperlukan
+# Import modul yang diperlukan
 from flask import Flask, render_template, Response, request, jsonify, send_file
 from aiortc import RTCPeerConnection, RTCSessionDescription
 import cv2
@@ -6,21 +6,20 @@ import time
 import uuid
 import asyncio
 import logging
+import time
+from ultralytics import YOLO
 import numpy as np
 import threading
 import os
 import csv
 import pandas as pd
-from ultralytics import YOLO
 
-# Membuat instance aplikasi Flask
+# Buat instance aplikasi Flask
 app = Flask(__name__, static_url_path='/static')
 
-# Mengatur untuk melacak instance RTCPeerConnection
+# Atur untuk melacak instance RTCPeerConnection
 pcs = set()
 
-# Mendefinisikan jalur untuk model, mask, dan laporan
-# Mask : Hitam 0, Putih 1
 path_to_model = "/Users/macbook/crowd-detection-web/yolov8n.pt"
 path_to_mask = "/Users/macbook/crowd-detection-web/mask.jpg"
 path_to_report = "/Users/macbook/crowd-detection-web/report.csv"
@@ -35,7 +34,7 @@ mask = cv2.imread(path_to_mask, cv2.IMREAD_GRAYSCALE)
 kondisi = "Proses"
 area = mask.copy()
 
-# Fungsi untuk menggambar kotak deteksi pada frame
+# Fungsi draw_boxes
 def draw_boxes(result, frame):
     blank = np.zeros(frame.shape, dtype=np.uint8)
     image = frame.copy()
@@ -44,7 +43,7 @@ def draw_boxes(result, frame):
     # Looping melalui hasil deteksi
     for box in result.boxes:
         x1, y1, x2, y2 = [
-            round(x) for x in box.xyxy[0].tolist()
+        round(x) for x in box.xyxy[0].tolist()
         ]
         class_id = box.cls[0].item()
         prob = round(box.conf[0].item(), 2)
@@ -68,7 +67,7 @@ def draw_boxes(result, frame):
         cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
         cv2.rectangle(blank, (x1, y1), (x2, y2), (255, 255, 255), -1)
 
-    # Menggunakan Mask dan Konversi Ke grayscale
+    # Menggunakan Mask dan Konversi Warna
     blank = cv2.bitwise_and(blank, blank, mask=mask)
     blank = cv2.cvtColor(blank, cv2.COLOR_BGR2GRAY)
 
@@ -76,38 +75,46 @@ def draw_boxes(result, frame):
     sum_object = [car, motorcycle, person]
     return blank, image, sum_object
 
-# Fungsi untuk mendeteksi objek dalam frame
+# Fungsi Draw
+def draw(result, frame):
+    blank = frame.copy()
+    for box in result.boxes:
+        x1, y1, x2, y2 = [
+        round(x) for x in box.xyxy[0].tolist()
+        ]
+        # for every box draw a rectangle for contours
+        cv2.rectangle(blank, (x1, y1), (x2, y2), (255, 255, 255), 2)
+    return blank
+
+# Fungsi Detect
 def detect(frame, show=True):
     image = frame.copy()
     masked = cv2.bitwise_and(image, image, mask=mask)
     result = model(masked, conf=0.25, verbose=False)
-    area, image, object = draw_boxes(result[0], image)
+    area , image, object= draw_boxes(result[0], image)
     percentage = round((np.sum(area) / np.sum(mask)) * 100, 2)
     return image, percentage, object
-
-# Fungsi untuk menghasilkan frame yang akan dikirimkan ke klien
+    
+# Fungsi generate_frames
 def generate_frames():
     while True:
         global area
         ret, buffer = cv2.imencode('.jpg', area)
         frame = buffer.tobytes()
-
-        # Menggabungkan frame dan mengirimkan untuk streaming
+        # Concatenate frame and yield for streaming
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-# Fungsi untuk menulis data dengan header ke file CSV
+# Fungsi write_data_with_header
 def write_data_with_header(path_to_report, datetime, sum_car, sum_motor, sum_person, sum_percentage, kondisi):
-
-    # Memeriksa apakah file ada dan jika kosong
+    # Check if the file exists and if it's empty
     if not os.path.exists(path_to_report) or os.stat(path_to_report).st_size == 0:
         with open(path_to_report, "w") as f:
-
-            # Menulis header
+            # Write the header
             f.write("Tanggal/Jam,Jumlah Mobil,Jumlah Motor,Jumlah Orang,Persentase Area,Kondisi\n")
-    with open(path_to_report, "a", newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow([datetime, int(np.mean(sum_car)), int(np.mean(sum_motor)), int(np.mean(sum_person)), np.mean(sum_percentage), kondisi])
+        with open(path_to_report, "a", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([datetime, int(np.mean(sum_car)), int(np.mean(sum_motor)), int(np.mean(sum_person)), np.mean(sum_percentage), kondisi])
 
 # Inisialisasi Kamera dan Variabel Waktu
 def run_yolo():
@@ -139,7 +146,8 @@ def run_yolo():
             area = cv2.putText(area, f"FPS: {1/elapsed_time:.2f}", (7, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
             area = cv2.putText(area, f"Kondisi: {kondisi}", (7, 45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
-            # Mengumpulkan data statistik selama interval waktu tertentu
+            # bertujuan untuk mengumpulkan data statistik selama interval waktu tertentu.
+            # setiap berapa detik update persentase
             if time.time() - time_percentage > 1:
                 sum_percentage.append(percentage)
                 sum_car.append(object[2])
@@ -147,8 +155,8 @@ def run_yolo():
                 sum_person.append(object[0])
                 time_percentage = time.time()
 
-            # Setiap berapa detik update kondisi lalu simpan ke CSV
-            if time.time() - time_area > 5:  # Kurangi interval waktu menjadi 5 detik
+            # setiap berapa detik update kondisi lalu simpan ke csv
+            if time.time() - time_area > 5:  # kurangi interval waktu menjadi 5 detik
                 if np.mean(sum_percentage) > 30:
                     kondisi = "Ramai"
                 else:
@@ -160,31 +168,41 @@ def run_yolo():
                 # Tulis data dengan header
                 write_data_with_header(path_to_report, datetime, sum_car, sum_motor, sum_person, sum_percentage, kondisi)
 
-                # Kosongkan list setelah menulis data
+                # Reset daftar setelah menulis ke CSV
                 sum_car.clear()
                 sum_motor.clear()
                 sum_person.clear()
                 sum_percentage.clear()
+
                 time_area = time.time()
 
-# Route untuk merender halaman index
+def write_data_with_header(path_to_report, datetime, sum_car, sum_motor, sum_person, sum_percentage, kondisi):
+    header_needed = not os.path.exists(path_to_report) or os.stat(path_to_report).st_size == 0
+
+    with open(path_to_report, "a", newline='') as f:
+        writer = csv.writer(f)
+        if header_needed:
+            writer.writerow(["Tanggal/Jam", "Jumlah Mobil", "Jumlah Motor", "Jumlah Orang", "Persentase Area", "Kondisi"])
+        writer.writerow([datetime, int(np.mean(sum_car)), int(np.mean(sum_motor)), int(np.mean(sum_person)), np.mean(sum_percentage), kondisi])
+
+# Route untuk merender template HTML
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-# Route untuk mengecek kondisi keramaian
-@app.route("/kondisi", methods=["GET"])
+    global kondisi
+    return render_template('index.html', kondisi = kondisi)
+    # return redirect(url_for('video_feed')) 
+    #to render live stream directly
+    
+@app.route("/kondisi", methods = ["GET"])
 def cek_kondisi():
     global kondisi
     return kondisi
 
-# Route untuk mendownload file laporan CSV
 @app.route('/download')
 def download():
     path = path_to_report
     return send_file(path, as_attachment=True)
 
-# Route untuk menampilkan data dari file CSV
 @app.route('/show_data')
 def showData():
     data_file_path = path_to_report
@@ -193,45 +211,50 @@ def showData():
     uploaded_df_html = uploaded_df.to_html(classes='table table-striped table-bordered', index=False)
     return render_template('show_csv_data.html', data_var=uploaded_df_html)
 
-# Fungsi asynchronous untuk menangani pertukaran offer
+# Fungsi offer_async()
+# Asynchronous function to handle offer exchange
 async def offer_async():
     params = await request.json
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    # Membuat instance RTCPeerConnection
+    # Create an RTCPeerConnection instance
     pc = RTCPeerConnection()
 
-    # Menghasilkan ID unik untuk RTCPeerConnection
+    # Generate a unique ID for the RTCPeerConnection
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pc_id = pc_id[:8]
 
-    # Membuat dan mengatur deskripsi lokal
+    # Create a data channel named "chat"
+    # pc.createDataChannel("chat")
+
+    # Create and set the local description
     await pc.createOffer(offer)
     await pc.setLocalDescription(offer)
 
-    # Menyiapkan data respons dengan SDP dan tipe lokal
+    # Prepare the response data with local SDP and type
     response_data = {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
 
     return jsonify(response_data)
 
-# Fungsi pembungkus untuk menjalankan fungsi offer_async secara asynchronous
+# Fungsi offer()
+# Wrapper function for running the asynchronous offer function
 def offer():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     future = asyncio.run_coroutine_threadsafe(offer_async(), loop)
     return future.result()
 
-# Route untuk menangani permintaan offer
+# Route to handle the offer request
 @app.route('/offer', methods=['POST'])
 def offer_route():
     return offer()
 
-# Route untuk streaming video frame
+# Route to stream video frames
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Jalankan Aplikasi Flask
 if __name__ == "__main__":
-    threading.Thread(target=run_yolo).start()  # Mulai thread untuk menjalankan deteksi YOLOv8 dan pencatatan
-    app.run(debug=True, host='0.0.0.0')  # Jalankan aplikasi Flask dengan debugging diaktifkan
+    threading.Thread(target = run_yolo).start()
+    app.run(debug=True, host='0.0.0.0')
